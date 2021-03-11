ac0502a4f9798048a3554d0faa3ea5b869276147
wad0 <fukushima0102@gmail.com>
2021-03-11 06:30:48 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def new
+    @book=Book.new
+  end
+
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to'/top'
+  end
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+end
