124a9d7c28446c86d32112bd1263e61a84c54bcd
wad0 <fukushima0102@gmail.com>
2021-03-12 02:14:58 +0000

    0312_1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < ApplicationController
+
+  def index
+    @books=Book.all
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
   def new
     @book=Book.new
   end
@@ -6,9 +15,24 @@ class BooksController < ApplicationController
   def create
     book=Book.new(book_params)
     book.save
-    redirect_to'/top'
+    redirect_to book_path(blog.id)
   end
 
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(blog)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
 
   private
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,5 @@
 class HomesController < ApplicationController
-  def top
-  end
-  
   def index
-  end
 
-  def show
-  end
-
-  def new
-  end
-
-  def edit
   end
 end
