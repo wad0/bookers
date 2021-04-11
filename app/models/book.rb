8787252cce40aca4fee07e0cca779a53900e8db2
wad0 <fukushima0102@gmail.com>
2021-04-11 13:23:38 +0000

    0411_1

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+
+    has_many :favorites,dependent: :destroy
+
 validates :title, presence: true
 validates :body, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_063416) do
+ActiveRecord::Schema.define(version: 2021_04_11_105019) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
